e76ed007aaf51567af216342ff2078b26aaf9d23
hhjin015 <heo015414@gmail.com>
2020-10-21 14:23:44 +0900

    Updates

---
+++ precache-manifest.bf72e0b30de8f234f731a954b03d936f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19801e138c9a5dde1b1d8815aa48cad7",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "a61665086e90827e2d39",
+    "url": "/movie_app/static/css/main.22976fef.chunk.css"
+  },
+  {
+    "revision": "b9cb88537a0f4167021b",
+    "url": "/movie_app/static/js/2.1c14cd22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.1c14cd22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61665086e90827e2d39",
+    "url": "/movie_app/static/js/main.a14875e9.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
